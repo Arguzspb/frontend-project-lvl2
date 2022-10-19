7eda1d1f0d859618bcedbc8c57916a9f097cf835
Dmitriy <arguzspbgames@mail.ru>
2022-10-19 21:46:12 +0300

    added jest and lint tests --fix

---
+++ __tests__/plain_test.js
@@ -0,0 +1,14 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import gendiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('plain object', () => {
+  const resultPlain = readFile('plainTestResult.txt');
+  expect(gendiff('file1.json', 'file2.json')).toEqual(resultPlain);
+});

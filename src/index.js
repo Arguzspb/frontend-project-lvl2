97b948f2e72f3c668bc7b851ec6cae2d0d1ba87d
Dmitriy <arguzspbgames@mail.ru>
2024-03-04 23:09:23 +0300

    step 9

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import formatter from './formatters/index.js';
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getAbsolutePath = (fileName) => path
   .join(`${__dirname}`, '..', '__fixtures__', fileName);
-const getFileData = (filepath) => readFileSync(filepath, 'utf-8');
+const getFileData = (filepath) => fs.readFileSync(path.resolve(filepath), 'utf-8');
 const getExtension = (filepath) => path.extname(filepath).slice(1);
 
 const gendiff = (filepath1, filepath2, formatType = 'stylish') => {

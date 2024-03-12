a7e707c06c3864a5a05cb8dbb21ebc362b914f7e
Dmitriy <arguzspbgames@mail.ru>
2024-03-12 16:29:59 +0300

    step 9

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
+import { Command } from 'commander';
 import gendiff from '../src/index.js';
+
+const program = new Command();
 
 program
   .name('gendiff')
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
-  .argument('<filepath1>', 'path to file1')
-  .argument('<filepath2>', 'path to file2')
+  .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format (default "stylish")')
-  .action((filepath1, filepath2, options) => {
-    console.log(gendiff(filepath1, filepath2, options.format));
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2, program.opts().format));
   });
-program.parse(process.argv);
+program.parse();
--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import fs from 'fs';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
+import path from 'path';
 import formatter from './formatters/index.js';
 import buildDiff from './buildDiff.js';
 import parseData from './parser.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const getAbsolutePath = (fileName) => path
-  .join(`${__dirname}`, '..', '__fixtures__', fileName);
-const getFileData = (filepath) => fs.readFileSync(path.resolve(filepath), 'utf-8');
 const getExtension = (filepath) => path.extname(filepath).slice(1);
 
+const readFile = (filepath) => fs.readFileSync(path.resolve(filepath), { encoding: 'utf8', flag: 'r' });
+
 const gendiff = (filepath1, filepath2, formatType = 'stylish') => {
-  const absPath1 = getAbsolutePath(filepath1);
-  const absPath2 = getAbsolutePath(filepath2);
-  const fileDataA = parseData(getFileData(absPath1), getExtension(absPath1));
-  const fileDataB = parseData(getFileData(absPath2), getExtension(absPath2));
+  const fileDataA = parseData(readFile(filepath1), getExtension(filepath1));
+  const fileDataB = parseData(readFile(filepath2), getExtension(filepath2));
   const diffTree = buildDiff(fileDataA, fileDataB);
   const formatedDiff = formatter(diffTree, formatType);
   return formatedDiff;
